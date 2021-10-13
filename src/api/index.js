ff4f0db58ad02eaabd475b1b6118deefc37ac578
pradeepx <pradeep.suthar@blitzscale.co>
2021-10-13 13:22:48 +0530

    otp api created

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const emojis = require('./emojis')
 
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -9,6 +10,37 @@ router.get('/', (req, res) => {
     message: 'API - 👋🌎🌍🌏'
   })
 })
+
+router.get('/verify-mobile-number', (req, res) => {
+  console.log(req.body.mobile_number)
+
+  res.json({
+    message: `${req.body.mobile_number} : verified`
+  }).statusCode(200)
+
+})
+
+router.get('/get-otp', (req, res) => {
+  
+  let otp = (Math.random()*10000).toString().substring(0,4)
+  
+  res.json({
+    message: 'opt-genrated successfully',
+    otp: otp
+  }).statusCode(200)
+
+})
+
+router.get('/verify-otp', (req, res) => {
+  console.log(req.body.otp)
+
+  res.json({
+    message: `wellcome, otp verified (${req.body.otp})`
+  }).statusCode(200)
+
+})
+
+
 
 router.use('/emojis', emojis)
 
